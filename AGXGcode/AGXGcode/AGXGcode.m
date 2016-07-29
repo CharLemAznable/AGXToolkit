ed3f339b5c7738ab1f974659b9bd561b92c3933c
CharLemAznable <545541819@qq.com>
2016-07-29 17:39:13 +0800

    Fixed deployment target.

---
+++ AGXGcode/AGXGcode/AGXGcode.m
@@ -0,0 +1,13 @@
+//
+//  AGXGcode.m
+//  AGXGcode
+//
+//  Created by Char Aznable on 16/7/26.
+//  Copyright © 2016年 AI-CUC-EC. All rights reserved.
+//
+
+#import "AGXGcode.h"
+
+@implementation AGXGcode
+
+@end
---
+++ AGXGcode/AGXGcode/Components/AGXDecodeHints.m
@@ -0,0 +1,13 @@
+//
+//  AGXDecodeHints.m
+//  AGXGcode
+//
+//  Created by Char Aznable on 16/7/26.
+//  Copyright © 2016年 AI-CUC-EC. All rights reserved.
+//
+
+#import "AGXDecodeHints.h"
+
+@implementation AGXDecodeHints
+
+@end
