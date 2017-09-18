f6b2a3bbe106a4efe1b2a68c3f90b6e5e4a7ba0f
CharLemAznable <545541819@qq.com>
2017-09-18 10:16:46 +0800

    Add 10.3 adaption.

--- AGXCore/AGXCore/Constants/AGXAdapt.h
+++ AGXCore/AGXCore/Constants/AGXAdapt.h
@@ -43,6 +43,7 @@ CGSizeEqualToSize(CGSizeMake((width), (height)), [[UIScreen mainScreen] currentM
 #define AGX_BEFORE_IOS10_0              (AGX_SYSTEM_VERSION_COMPARE("10.0") == NSOrderedAscending)
 #define AGX_BEFORE_IOS10_1              (AGX_SYSTEM_VERSION_COMPARE("10.1") == NSOrderedAscending)
 #define AGX_BEFORE_IOS10_2              (AGX_SYSTEM_VERSION_COMPARE("10.2") == NSOrderedAscending)
+#define AGX_BEFORE_IOS10_3              (AGX_SYSTEM_VERSION_COMPARE("10.3") == NSOrderedAscending)
 
 #define AGX_IOS8_1_OR_LATER             (!AGX_BEFORE_IOS8_1)
 #define AGX_IOS8_2_OR_LATER             (!AGX_BEFORE_IOS8_2)
@@ -55,6 +56,7 @@ CGSizeEqualToSize(CGSizeMake((width), (height)), [[UIScreen mainScreen] currentM
 #define AGX_IOS10_0_OR_LATER            (!AGX_BEFORE_IOS10_0)
 #define AGX_IOS10_1_OR_LATER            (!AGX_BEFORE_IOS10_1)
 #define AGX_IOS10_2_OR_LATER            (!AGX_BEFORE_IOS10_2)
+#define AGX_IOS10_3_OR_LATER            (!AGX_BEFORE_IOS10_3)
 
 #define AGXCalendarUnitEra              NSCalendarUnitEra
 #define AGXCalendarUnitYear             NSCalendarUnitYear
