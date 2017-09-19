40df654448f439e6034852fe1e7f5a08b5835144
CharLemAznable <545541819@qq.com>
2017-09-19 16:00:31 +0800

    Add AGXCrash.

---
+++ AGXCrash/AGXCrash/AGXCrash.h
@@ -0,0 +1,13 @@
+//
+//  AGXCrash.h
+//  AGXCrash
+//
+//  Created by Char Aznable on 17/9/19.
+//  Copyright © 2017年 AI-CUC-EC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGXCrash : NSObject
+
+@end
---
+++ AGXCrash/AGXCrash/AGXCrash.m
@@ -0,0 +1,13 @@
+//
+//  AGXCrash.m
+//  AGXCrash
+//
+//  Created by Char Aznable on 17/9/19.
+//  Copyright © 2017年 AI-CUC-EC. All rights reserved.
+//
+
+#import "AGXCrash.h"
+
+@implementation AGXCrash
+
+@end
