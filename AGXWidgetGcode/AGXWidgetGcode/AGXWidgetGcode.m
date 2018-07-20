5b3592f06126ab5f3ba4f1853365ac8cb2ff98f4
CharLemAznable <545541819@qq.com>
2018-07-20 14:28:58 +0800

    Change supported only on iOS 10 and above.

--- AGXWidgetGcode/AGXWidgetGcode/AGXWidgetGcode.m
+++ AGXWidgetGcode/AGXWidgetGcode/AGXWidgetGcode.m
@@ -8,5 +8,5 @@
 
 #import "AGXWidgetGcode.h"
 
-const long AGXWidgetGcodeVersionNumber = AGXToolkitVersionNumber;
-const unsigned char AGXWidgetGcodeVersionString[] = AGXToolkitVersionString;
+const long AGXWidgetGcodeVersionNumber = 10000;
+const unsigned char AGXWidgetGcodeVersionString[] = "1.0.0";
